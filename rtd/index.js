bf629c936237e23a8a1ae5c7a968455253aac0c5
acatejr@gmail.com <acatejr@gmail.com>
2025-01-03 16:49:47 -0700

    Finished Adding hover styles to buttons

--- rtd/index.js
+++ rtd/index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js"
-import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-ec3de-default-rtdb.firebaseio.com/"
@@ -10,8 +10,49 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
+const shoppingListEl = document.getElementById("shopping-list")
+
+onValue(shoppingListInDB, (snapshot) => {
+
+    if (snapshot.exists()) {
+
+        let itemsArray = Object.entries(snapshot.val())
+        clearShoppingListEl()
+
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            appendItemToShoppingListEl(currentItem)
+        }
+    } else {
+        shoppingListEl.innerHTML = "No items here..., yet."
+    }
+
+})
+
+const clearShoppingListEl = () => {
+    shoppingListEl.innerHTML = ""
+}
+
+const clearInputFieldEl = () => {
+    inputFieldEl.value = ""
+}
+
+const appendItemToShoppingListEl = (item) => {
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")
+    newEl.textContent = itemValue
+    newEl.addEventListener("click", () => {
+        let path = `shoppingList/${itemID}`
+        let exactLocationOfItemInDB = ref(database, path)
+        remove(exactLocationOfItemInDB)
+    })
+    shoppingListEl.append(newEl)
+}
 
 addButtonEl.addEventListener("click", () => {
     let inputVal = inputFieldEl.value
     push(shoppingListInDB, inputVal)
+    clearInputFieldEl()
 })
